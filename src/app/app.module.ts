b35452091ee501e7e9281c8cfe9de6450258b2d9
aperezbell <alvaroperezbellon@gmail.com>
2022-06-11 19:11:17 +0200

    Creada la estructura web. Falta adaptarlo al estilo Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HeaderWebComponent } from './components/header-web/header-web.component';
+import { FooterWebComponent } from './components/footer-web/footer-web.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderWebComponent,
+    FooterWebComponent,
   ],
   imports: [
     BrowserModule
