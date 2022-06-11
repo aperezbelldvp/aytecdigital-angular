b82597334697d6265be53f2196f9a8edbf7e4ba8
aperezbell <alvaroperezbellon@gmail.com>
2022-06-11 19:15:43 +0200

    Creados los componentes y creada la carpeta de models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderWebComponent } from './components/header-web/header-web.component';
 import { FooterWebComponent } from './components/footer-web/footer-web.component';
+import { CardListComponent } from './components/card-list/card-list.component';
+import { CardContactComponent } from './components/card-contact/card-contact.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderWebComponent,
     FooterWebComponent,
+    CardListComponent,
+    CardContactComponent,
   ],
   imports: [
     BrowserModule
